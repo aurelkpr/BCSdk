36334d41be3efebacf92f3f0f4b62f1ae7e9bbdf
Aurel Chiper <chiper.aurel@gmail.com>
2022-02-08 13:32:40 +0200

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# react-native-bc-sdk
+
+Test lib for BC project
+
+## Installation
+
+```sh
+npm install react-native-bc-sdk
+```
+
+## Usage
+
+```js
+import { multiply } from "react-native-bc-sdk";
+
+// ...
+
+const result = await multiply(3, 7);
+```
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import { NativeModules, Platform } from 'react-native';
+
+const LINKING_ERROR =
+  `The package 'react-native-bc-sdk' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo managed workflow\n';
+
+const BcSdk = NativeModules.BcSdk
+  ? NativeModules.BcSdk
+  : new Proxy(
+      {},
+      {
+        get() {
+          throw new Error(LINKING_ERROR);
+        },
+      }
+    );
+
+export function multiply(a: number, b: number): Promise<number> {
+  return BcSdk.multiply(a, b);
+}
